fc5fd6bfed1788a2c7a3c7678c8c41e707f07ac9
Ice Xortilla <ice.xortilla@gmail.com>
2017-02-20 10:52:41 -0500

    Simple BinarySearch Implementation

---
+++ BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    enum SearchResult
+    {
+        Found,
+        NotFound
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 0, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int key = 5;
+
+            Console.WriteLine(search(a, key));
+            Console.WriteLine(search(a, -1));;
+            Console.ReadLine();
+
+        }
+
+
+        public static SearchResult search(int[] a,int searchkey)
+        {
+
+            int low = 0;
+            int high = a.Length - 1;
+            int mid;
+
+            while (low <= high)
+            {
+                mid = (low + high) / 2;
+                if (a[mid].CompareTo(searchkey) < 0)
+                    low = mid + 1;
+                else if (a[mid].CompareTo(searchkey) > 0)
+                    high = mid - 1;
+                else
+                    return SearchResult.Found;
+
+            }
+
+
+            return SearchResult.NotFound;
+
+        }
+    }
+}
